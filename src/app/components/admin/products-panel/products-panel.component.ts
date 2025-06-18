ef938068458474ca07e9a38dfcd1324d37d43310
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-06-17 22:55:52 -0500

    feat: component to products panel

---
+++ src/app/components/admin/products-panel/products-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-panel',
+  imports: [],
+  templateUrl: './products-panel.component.html',
+  standalone: true,
+  styleUrl: './products-panel.component.css'
+})
+export class ProductsPanelComponent {
+
+}
