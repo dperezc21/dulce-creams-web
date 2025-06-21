593b705b2694b06109221a38f42c776408869f28
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-06-21 14:08:05 -0500

    feat: service to show snack bar like messagee

--- src/app/controllers/product.controller.ts
+++ src/app/controllers/product.controller.ts
@@ -1,17 +1,22 @@
 import {Injectable, signal, WritableSignal} from '@angular/core';
-import {BehaviorSubject, Observable, of, tap} from 'rxjs';
+import {of, tap} from 'rxjs';
 import {Product} from '../interfaces/product';
-
+import {SnackBarService} from '../services/snack-bar.service';
 
 @Injectable({ providedIn: "root" })
 export class ProductController {
 
   private productsBS = signal<Product[]>([]);
 
+  constructor(private snackBarService: SnackBarService) {
+  }
+
   saveProduct(productToSave: Product): void {
     of(true)
-       .pipe(tap(value => this.addNewProduct(productToSave)))
-       .subscribe();
+       .pipe(tap(value => {
+         this.addNewProduct(productToSave);
+         this.snackBarService.showMessage("producto guardado");
+       })).subscribe();
   }
 
   setProducts(productList: Product[]): void {
