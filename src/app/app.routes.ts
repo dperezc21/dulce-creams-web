f9503693b014723b21a5bfb7a02c4824997a0777
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-06-25 13:03:00 -0500

    feat: orders component created and order button added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import {Routes} from '@angular/router';
 import {adminRoutes} from './routes/admin.routes';
+import {OrdersComponent} from './components/orders/orders.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: OrdersComponent
+  },
   {
     path: "panel",
     loadComponent: () => import('./components/admin/panel/panel.component').then(value => value.PanelComponent),
