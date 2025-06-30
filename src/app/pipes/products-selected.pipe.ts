6f4db98c0073abb3c34a2a0ce5656268fe4d0ecf
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-06-30 12:41:14 -0500

    feat: pipe to valid which products were selected

---
+++ src/app/pipes/products-selected.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Product} from '../interfaces/product';
+
+@Pipe({
+  standalone: true,
+  name: 'validProductsSelected'
+})
+export class ValidProductsSelectedPipe implements PipeTransform {
+
+  transform(currentProduct: Product, productsSelected: Product[]): unknown {
+    return productsSelected.findIndex((product: Product) => product.id === currentProduct.id) !== -1;
+  }
+
+}
